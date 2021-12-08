b214914c24b159fb6df5f0ae467416a3e5ffe80c
Marentez Delgado Francisco Jared <20170104@uttcampus.edu.mx>
2021-12-07 21:31:26 -0600

    Modelo y Adaptador para opciones de Humo, Temperatura, Humedad

--- proyecto/app/src/main/java/com/example/proyecto/MainActivity.java
+++ proyecto/app/src/main/java/com/example/proyecto/MainActivity.java
@@ -1,9 +1,19 @@
 package com.example.proyecto;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+
+import com.example.proyecto.models.modelo;
+import com.example.proyecto.cosasrecycler.adaptator;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     RecyclerView reci;
@@ -11,6 +21,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setContentView(R.id.reciclador);
+
+        iniciar();
     }
+
+    public void iniciar(){
+
+        List<modelo> elements=new ArrayList<>();
+
+
+        elements.add(new modelo("Temperatura",new Intent(this,Temperatura.class)));
+        elements.add(new modelo("Humedad",new Intent(this,Temperatura.class)));
+        elements.add(new modelo("Humo",new Intent(this,Temperatura.class)));
+
+        adaptator root=new adaptator(elements, this, new adaptator.OnItemClickListener() {
+            @Override
+            public void OnItemClick(adaptator view) {
+
+            }
+        });
+
+
+        RecyclerView recyclerView= findViewById(R.id.reciclador);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(root);
+    }
+
 }
--- proyecto/app/src/main/java/com/example/proyecto/cosasrecycler/adaptator.java
+++ proyecto/app/src/main/java/com/example/proyecto/cosasrecycler/adaptator.java
@@ -1,33 +1,76 @@
 package com.example.proyecto.cosasrecycler;
 
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.proyecto.MainActivity;
 import com.example.proyecto.R;
+import com.example.proyecto.models.modelo;
+
+import java.util.List;
 
 public class adaptator extends RecyclerView.Adapter<adaptator.ViewHolder> {
+
+    private List<modelo> ListModel;
+    private LayoutInflater inflater;
+    private Context context;
+    final adaptator.OnItemClickListener listener;
+
+    public adaptator(List<modelo> listModel, Context context, adaptator.OnItemClickListener listener) {
+        ListModel = listModel;
+        this.inflater=LayoutInflater.from(context);
+        this.context = context;
+        this.listener = listener;
+    }
+
     @NonNull
     @Override
     public adaptator.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view=inflater.inflate(R.layout.item, null);
+        return new adaptator.ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull adaptator.ViewHolder holder, int position) {
-
+        holder.binData(ListModel.get(position));
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return ListModel.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
+    public interface OnItemClickListener {
+        void OnItemClick (adaptator view);
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+        TextView Nombre;
+        Intent I;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            Nombre=itemView.findViewById(R.id.sensor);
+            itemView.setOnClickListener(this);
         }
+
+        public void binData(modelo modelo) {
+            Nombre.setText(modelo.getNombre());
+            I=modelo.getAccion();
+        }
+        @Override
+        public void onClick(View v) {
+            v.getContext().startActivity(I);
+        }
+
+
     }
+
 }
