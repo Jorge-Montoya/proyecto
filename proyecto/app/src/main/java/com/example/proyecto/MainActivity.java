d7d6d03cdef75f50dcfd5049428c7de2b6852c98
Marentez Delgado Francisco Jared <20170104@uttcampus.edu.mx>
2021-12-08 12:27:16 -0600

    Evento Led encender/apagar modelo singleton

--- proyecto/app/src/main/java/com/example/proyecto/MainActivity.java
+++ proyecto/app/src/main/java/com/example/proyecto/MainActivity.java
@@ -8,24 +8,35 @@ import android.Manifest;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Switch;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.example.proyecto.models.modelo;
 import com.example.proyecto.cosasrecycler.adaptator;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
+    Switch aSwitch, bSwitch;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);/*
-<<<<<<< HEAD
-=======
-*/
-        iniciar();/*
->>>>>>> b214914c24b159fb6df5f0ae467416a3e5ffe80c*/
+        setContentView(R.layout.activity_main);
+        aSwitch=findViewById(R.id.luces);
+        iniciar();
     }
 
     public void iniciar(){
@@ -40,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
 
         adaptator root=new adaptator(elements, this, new adaptator.OnItemClickListener() {
             @Override
-            public void OnItemClick(adaptator view) {
+           public void OnItemClick(adaptator view) {
 
             }
         });
@@ -52,4 +63,70 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setAdapter(root);
     }
 
+    public void onclick(View view) {
+        if (view.getId()==R.id.luces){
+            if (aSwitch.isChecked()){
+                String url = "https://io.adafruit.com/api/v2/Jared_Marentez/feeds/led/data";
+                JSONObject datos = new JSONObject();
+                try {
+                    datos.put("value", "ON");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                JsonObjectRequest sendData = new JsonObjectRequest(Request.Method.POST, url, datos, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Toast.makeText(getApplicationContext(), "Luces encendidas", Toast.LENGTH_SHORT).show();
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getApplicationContext(), error.toString(), Toast.LENGTH_SHORT).show();
+
+
+                    }
+                }) {
+                    @Override
+                    public Map<String, String> getHeaders() throws AuthFailureError {
+                        HashMap<String, String> headers = new HashMap<String, String>();
+                        headers.put("X-AIO-Key","aio_EDbk824WyerjDic1ewTuAA4eYjOI");
+                        return headers;
+                    }
+                };
+
+                SingletonRequest.getInstance(this).addToRequestQue(sendData);
+            } else{
+                String url = "https://io.adafruit.com/api/v2/Jared_Marentez/feeds/led/data";
+                JSONObject datos = new JSONObject();
+                try {
+                    datos.put("value", "OFF");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                JsonObjectRequest sendData = new JsonObjectRequest(Request.Method.POST, url, datos, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Toast.makeText(getApplicationContext(), "Luces apagadas", Toast.LENGTH_SHORT).show();
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getApplicationContext(), error.toString(), Toast.LENGTH_SHORT).show();
+
+
+                    }
+                }) {
+                    @Override
+                    public Map<String, String> getHeaders() throws AuthFailureError {
+                        HashMap<String, String> headers = new HashMap<String, String>();
+                        headers.put("X-AIO-Key","aio_EDbk824WyerjDic1ewTuAA4eYjOI");
+                        return headers;
+                    }
+                };
+                SingletonRequest.getInstance(this).addToRequestQue(sendData);
+            }
+        }
+    }
 }
