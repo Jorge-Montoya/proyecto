fb88d19d6db3074f42c7f6d41f7ae97b70b8a803
Jorge_Montoya <jorgemontoya192@gmail.com>
2021-12-09 10:41:45 -0600

    campos de value y feed en activitys de sensores

--- modificacion jorge/proyecto/app/src/main/java/com/example/proyecto/Temperatura.java
+++ modificacion jorge/proyecto/app/src/main/java/com/example/proyecto/Temperatura.java
@@ -3,12 +3,49 @@ package com.example.proyecto;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.google.gson.Gson;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class Temperatura extends AppCompatActivity {
-
+    TextView text1,text2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_temperatura);
+        text1.findViewById(R.id.elfeed_id2);
+        text2.findViewById(R.id.valor2);
+        sensortempe();
+    }
+    private void sensortempe() {
+        String link = "";
+        JSONObject datos = new JSONObject();
+        JsonObjectRequest registro = new JsonObjectRequest(Request.Method.GET, link, datos, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                final Gson gson = new Gson();
+                try{
+                    String a = response.getString("feed_id");
+                    String b = response.getString("value");
+                    text1.setText(a);
+                    text2.setText(b);
+                }catch(JSONException e){
+                    e.printStackTrace();
+
+                }}
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+            }
+
+        }
+        );
     }
 }
--- modificacion jorge/proyecto/app/src/main/java/com/example/proyecto/Ultrasonico.java
+++ modificacion jorge/proyecto/app/src/main/java/com/example/proyecto/Ultrasonico.java
@@ -3,12 +3,50 @@ package com.example.proyecto;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.google.gson.Gson;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
 
 public class Ultrasonico extends AppCompatActivity {
-
+    TextView txt1,txt2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ultrasonico);
+        txt1=findViewById(R.id.elfeed_id1);
+        txt2=findViewById(R.id.valor1);
+        sensorultra();
+    }
+    private void sensorultra() {
+        String link = "";
+        JSONObject datos = new JSONObject();
+        JsonObjectRequest registro = new JsonObjectRequest(Request.Method.GET, link, datos, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                final Gson gson = new Gson();
+                try{
+                String a = response.getString("feed_id");
+                String b = response.getString("value");
+                txt1.setText(a);
+                txt2.setText(b);
+            }catch(JSONException e){
+                e.printStackTrace();
+
+            }}
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+            }
+
+        }
+        );
     }
 }
